59ee939b58f3eabe41a90231fbeeecba20e70430
ASCRX <100628101+ASCRX@users.noreply.github.com>
2025-05-12 17:06:14 -0400

    Create jp_utils.py

---
+++ tasks/jp/jp_utils.py
@@ -0,0 +1,9 @@
+import evaluate, re, string
+from seqeval.metrics import f1_score
+
+def macro_f1_score(items):
+    unzipped_list = list(zip(*items))
+    golds = unzipped_list[0]
+    preds = unzipped_list[1]
+    fscore = f1_score(golds, preds, average="macro")
+    return fscore
